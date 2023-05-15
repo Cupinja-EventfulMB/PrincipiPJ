cb9e3f98dae830379462b16ab030f952dfd1e66d
Andrea <andrea.korunoska@student.um.si>
2023-05-15 16:16:41 +0200

    EMB-46 WebScraping

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+}
+
+group = "com.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    maven ( "https://jitpack.io" )
+}
+
+kotlin {
+    jvm {
+        jvmToolchain(11)
+        withJava()
+    }
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation(compose.desktop.currentOs)
+                implementation("it.skrape:skrapeit:1.2.2") // dodan skrape
+                implementation ("org.jsoup:jsoup:1.14.2") // jsoup
+                implementation("org.seleniumhq.selenium:selenium-java:3.8.1") // selenium
+
+            }
+        }
+        val jvmTest by getting
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "Podatki_PPJ_1"
+            packageVersion = "1.0.0"
+        }
+    }
+}
